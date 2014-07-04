40ed318459babeaf9cb52064a1016c33bd15de02
William Blankenship <william.jblankenship@gmail.com>
2014-07-04 12:30:05 -0500

    Added some verbose debugging to node-gyp check.
    Gives insight into why these things are failing in the wild

--- exercises/am_i_ready/exercise.js
+++ exercises/am_i_ready/exercise.js
@@ -228,9 +228,11 @@ function checkPython (pass, callback) {
 function checkNodeGyp (pass, callback) {
   // note we can't reliably trap stdout on Windows for `node-gyp -v`, perhaps because of the
   // immediate `process.exit(0)` after a `console.log(version)`?
-  child_process.exec('npm ls -g --depth 0', { env: process.env }, function (err, stdout, stderr) {
+  child_process.exec('nipm ls -g --depth 0', { env: process.env }, function (err, stdout, stderr) {
     if (err) {
-      exercise.emit('fail', '`' + chalk.bold('node-gyp') + '` not found in $PATH')
+      //Added some debugging to give insight into why things are failing.
+      exercise.emit('fail', '`' + chalk.bold('node-gyp') + '` not found in $PATH:\n'+err.stack)
+      exercise.emit(stderr)
       return callback(null, false)
     }
 
